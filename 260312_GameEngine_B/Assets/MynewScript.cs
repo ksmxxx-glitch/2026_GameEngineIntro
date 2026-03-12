acfa60d829b3fb59ea00c9af18fc9d19626ec3bf
ksmxxx-glitch <ksm071010@gmail.com>
2026-03-12 15:56:36 +0900

    유니티 캐릭터 조작 1

---
+++ 260312_GameEngine_B/Assets/MynewScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;  
+using System.Collections.Generic;   
+using UnityEngine;   
+
+public class MynewScript : MonoBehaviour
+{
+
+    public string playerName;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("æ»≥Á«œººø‰? " + playerName);
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
